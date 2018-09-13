f3531e69b64b2ef32948af25e4dad03b7ec75160
Sune Gynthersen <sune.gynthersen@gmail.com>
2018-09-13 16:44:24 +0200

    Simplify to the simplest possible thing

--- test.js
+++ test.js
@@ -1,14 +1,3 @@
-const testHelper1 = function () {
-  console.log('testHelper1 called');
+function testHelper1() {
   return 1;
 }
-
-const testHelper2 = function(param) {
-  console.log('testHelper2 called');
-
-  return 2 + param;
-}
-
-
-// call function 1
-testHelper1();
